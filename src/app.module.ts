9fe7fc3db16025cd51d7638cafdce593560bb6ff
Amr Mubarak <amrrdev@gmail.com>
2025-01-26 22:00:35 +0200

    refactor(app): configure TypeORM and ConfigModule for database and global settings

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,42 @@
-import { Module } from '@nestjs/common';
+import { Inject, Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CoffeesModule } from './coffees/coffees.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { CoffeeRatingModule } from './coffee-rating/coffee-rating.module';
+import { ConfigModule, ConfigType } from '@nestjs/config';
+import * as Joi from 'joi';
+import databaseConfig from './config/database.config';
 
 @Module({
-  imports: [CoffeesModule],
+  imports: [
+    CoffeesModule,
+    TypeOrmModule.forRootAsync({
+      useFactory: (databaseConfigration: ConfigType<typeof databaseConfig>) => ({
+        type: 'postgres',
+        host: databaseConfigration.host,
+        port: databaseConfigration.port,
+        username: databaseConfigration.username,
+        password: databaseConfigration.password,
+        database: databaseConfigration.database,
+        autoLoadEntities: true,
+        synchronize: true,
+      }),
+      inject: [databaseConfig.KEY],
+    }),
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        DATABASE_HOST: Joi.required(),
+        DATABASE_PORT: Joi.number().default(5432),
+        DATABASE_USERNAME: Joi.required(),
+        DATABASE_PASSWORD: Joi.required(),
+        DATABASE_NAME: Joi.required(),
+      }),
+      load: [databaseConfig],
+    }),
+    CoffeeRatingModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
