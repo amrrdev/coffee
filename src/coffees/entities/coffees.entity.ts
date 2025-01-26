843fdd9093df543d944a596fd57375ed7da0a9bc
Amr Mubarak <amrrdev@gmail.com>
2025-01-26 22:01:11 +0200

    feat(coffee): define Coffee entity with flavors relationship and recommendations

--- src/coffees/entities/coffees.entity.ts
+++ src/coffees/entities/coffees.entity.ts
@@ -1,6 +1,22 @@
+import { Column, Entity, Index, JoinTable, ManyToMany, PrimaryGeneratedColumn } from 'typeorm';
+import { Flavor } from './flavors.entity';
+
+@Entity()
 export class Coffee {
+  @PrimaryGeneratedColumn()
+  @Index()
   id: number;
+
+  @Column()
   name: string;
+
+  @Column()
   brand: string;
-  flavors: string[];
+
+  @Column({ default: 0 })
+  recommendations: number;
+
+  @JoinTable()
+  @ManyToMany(() => Flavor, flavor => flavor.coffees, { cascade: true })
+  flavors: Flavor[];
 }
